176a59183b0f8dfc787b49c4277fc2275194087e
m.sakamaki <mail@m-sakamaki.jp>
2015-08-20 14:15:47 +0900

    -

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ var port = process.env.PORT || 8001;
 app.use(express.static(__dirname + '/public'));
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
